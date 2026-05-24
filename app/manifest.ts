fe5aac2bb67d8bcfcc1017063e14b34caab6a056
Sadam-142 <ahmadsadamizzatulloh@gmail.com>
2026-05-24 18:52:29 +0700

    test

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,13 +15,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/UKMRISALAH.png",
         sizes: "192x192",
         type: "image/png",
-        purpose: "any maskable"
+        purpose: "any"
       },
       {
         src: "/UKMRISALAH.png",
         sizes: "512x512",
         type: "image/png",
-        purpose: "any maskable"
+        purpose: "any"
       },
     ],
   };
